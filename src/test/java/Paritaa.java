5688f697b7e5ed9c8fda6ba684e44d1083464909
Parita <paritacpatel@hotmail.com>
2021-02-15 00:59:10 -0500

    changes made

--- src/test/java/Paritaa.java
+++ src/test/java/Paritaa.java
@@ -1,5 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Paritaa {
     public static void main(String[] args) {
         System.out.println("parita");
+        WebDriver driver;
+        driver= new ChromeDriver();
     }
 }
